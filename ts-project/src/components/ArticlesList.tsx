14390f43cab687ae26437d0c7bd5f0d73166bb02
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-13 17:10:02 +0200

    button modified

--- ts-project/src/components/ArticlesList.tsx
+++ ts-project/src/components/ArticlesList.tsx
@@ -1,62 +1,64 @@
-import { useEffect, useState } from "react"
-import { Card, Container, Row,Col,Button } from "react-bootstrap"
-import {Article} from "../interfaces/Article"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
+import { Article } from "../interfaces/Article";
+import { Link } from "react-router-dom";
 const ArticleList = () => {
+  const [articles, setArticles] = useState<Article[]>([]);
 
-    
-    const [articles,setArticles] = useState<Article[]>([])
-
-    const fetchArticles = async () => {
-        try {
-            const response = await fetch("https://api.spaceflightnewsapi.net/v4/articles")
-            if(response.ok){
-                const data = await response.json()
-                setArticles(data.results)
-            }else{
-                console.log("error fetching articles")
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const fetchArticles = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spaceflightnewsapi.net/v4/articles"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setArticles(data.results);
+      } else {
+        console.log("error fetching articles");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() =>{
-        fetchArticles()
-    },[])
+  useEffect(() => {
+    fetchArticles();
+  }, []);
 
+  return (
+    <>
+      <Container>
+        <Row>
+          {articles &&
+            articles.map((article) => (
+              <Col xs={12} md={6} key={article.id} className="mb-2 text-center">
+                <Card
+                  className="d-flex flex-column rounded"
+                  style={{ height: 500, maxWidth: 500 }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={article.image_url}
+                    style={{ height: 200 }}
+                  />
+                  <Card.Body className="bg-info">
+                    <Card.Title>{article.title}</Card.Title>
+                    <Card.Text >
+                      {article.summary}
+                    </Card.Text>
+                    <Link to={"/details/" + article.id}>
+                      <Button variant="light" className="mt-auto border border-dark">
+                        Vedi Dettagli
+                      </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-    return(
-        <>
-            <Container>
-                <Row>
-        {articles && articles.map((article) => (
-                    <Col xs={12 }md={6} key={
-                        article.id
-                    } className="mb-2 text-center">
-            {/* <Card>
-                <Card.Body>{article.image_url}</Card.Body>
-                <Card.Title>{article.title}</Card.Title>
-                <Card.Text>{article.published_at}</Card.Text>
-            </Card> */}
-            <Card className="d-flex flex-column rounded" style={{height:500, width:500}}>
-      <Card.Img variant="top" src={article.image_url} style={{height:200}} />
-      <Card.Body className="bg-info">
-        <Card.Title>{article.title}</Card.Title>
-        <Card.Text className="fst-italic">
-          {article.summary}
-        </Card.Text>
-        <Link to={"/details/" + article.id} >
-        <Button variant="info" className="mt-auto">Vedi Dettagli</Button>
-        </Link>
-      </Card.Body>
-    </Card>
-                    </Col>
-                ))}
-                </Row>
-                </Container>
-        </>
-        )
-}
-
-export default ArticleList
+export default ArticleList;
